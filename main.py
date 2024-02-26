cd25f3ddcebb05c870118ebaf53ca4ee0b22b427
clorin <clorin@student.42.fr>
2024-02-26 22:43:10 +0100

    ok H vs H

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import time
 import tkinter as tk
+from tkinter import messagebox
+
+
+game_over = False
+board = {}
+captures = {"black": 0, "white": 0}
+current_player = ["black"]
 
 def draw_board(canvas):
+    canvas.delete("all")
     board_size = 19
     margin = 20
     cell_size = 30
@@ -35,7 +44,7 @@ def place_stone(canvas, board, x, y, player):
     stone_id = canvas.create_oval(center_x - radius, center_y - radius, center_x + radius, center_y + radius, fill=player)
     board[(x, y)] = {'color': player, 'id': stone_id}
 
-def triple(board, x, y, direction):
+def capture(board, x, y, direction):
     """
         return les stones capturés dans la direction demandé ou None
     """
@@ -52,7 +61,7 @@ def triple(board, x, y, direction):
                 if ((x + dx3, y + dy3)) in board:
                     ret = [board[x,y]['color'], board[x+dx1,y+dy1]['color'], board[x+dx2,y+dy2]['color'], board[x+dx3,y+dy3]['color']]
     if (len(ret) == 4):
-        if (ret[0] == ret[3] and ret[1] == ret[2]):
+        if (ret[0] == ret[3] and ret[1] == ret[2] and ret[0] != ret[1]):
             to_del1 = (x+dx1, y+dy1)
             to_del2 = (x+dx2, y+dy2)
             return (to_del1, to_del2) 
@@ -62,7 +71,7 @@ def check_capture(canvas, board, player, x, y, captures):
     captures_made = 0
     directions = [(1, 0),(-1, 0),(0, 1), (0, -1),(1, 1),(-1, -1),(-1, 1),(1, -1)]
     for dx, dy in directions:
-        captured = triple(board, x, y, (dx,dy))
+        captured = capture(board, x, y, (dx,dy))
         if captured:
             stone1 = captured[0]
             stone2 = captured[1]
@@ -76,98 +85,166 @@ def check_capture(canvas, board, player, x, y, captures):
 
     return captures_made
 
+def is_free(board, player, x, y, direction, length=6):
 
-def check_sequence(board, player, x, y, dx, dy):
-    """Vérifie une séquence de trois pierres dans une direction donnée."""
-    count = 0
-    for step in range(-3, 4):  # Vérifie 3 positions dans chaque direction à partir du point
-        nx, ny = x + dx * step, y + dy * step
-        if board.get((nx, ny), {}).get('color') == player:
-            count += 1
-            if count == 3:  # Trois pierres consécutives trouvées
-                print("sequence de trois")
-                return True
+    dx, dy = direction
+    sequence = []
+    three_free=[[0,1,1,1,0,0],[0,1,1,1,0,1],[0,1,1,1,0,2],[0,1,0,1,1,0],[0,1,1,0,1,0]]
+    for i in range(-1, length-1):  # Ajustez selon la longueur de la séquence que vous voulez vérifier
+        nx, ny = x + dx * i, y + dy * i
+        if 0 <= nx < 19 and 0 <= ny < 19:  # Assure que la position est dans les limites du plateau
+            stone = board.get((nx, ny))
+            if stone and stone['color'] == player:
+                sequence.append(1)  # Pierres de la même couleur
+            elif stone:
+                sequence.append(2)  # Autre couleur
+            else:
+                sequence.append(0)  # Aucune pierre ou pierre de couleur différente
         else:
-            count = 0  # Réinitialiser le compteur si la séquence est interrompue
-    return False
+            sequence.append(0)  # Hors du plateau pour les positions hors limites
+    return sequence in three_free
+        
 
 def check_double_three(board, player, x, y):
-    """Vérifie si le placement simulé crée un double-trois."""
-    directions = [(1, 0), (0, 1), (1, 1), (1, -1)]
-    # print(f"Check avec player = {player}, x = {x}, y = {y}")
+    directions = [(1, 0), (0, 1), (-1, 0), (0, -1),(1, 1),(-1, -1)]
     three_count = 0
-    board[(x, y)] = {'color': player}  # Simule le placement de la pierre
-    for dx, dy in directions:
-        if check_sequence(board, player, x, y, dx, dy):
+    for d in directions:
+        if is_free(board, player, x, y, d):
             three_count += 1
             if three_count > 1:
-                del board[(x, y)]  # Supprime la simulation
+                canvas.delete(board[x,y]['id'])
+                del board[x,y]
                 return True
-    del board[(x, y)]  # Supprime la simulation si non double-trois
     return False
 
+def restart_game():
+    global board, captures, current_player, game_over
+    board.clear()
+    captures = {"black": 0, "white": 0}
+    current_player = ["black"]
+    game_over = False
+    draw_board(canvas)
+    update_player_label()
+    update_capture_labels()
 
-def show_winner(canvas, winner):
-    canvas.create_text(400, 400, text=f"{winner.capitalize()} wins!", font=("Arial", 24), fill="red")
+def show_double_three_dialog():
+    dialog = tk.Toplevel(window)
+    dialog.title("Invalid Move")
+    dialog.geometry("300x100")  # Taille de la fenêtre
+    # Positionnement au centre de la fenêtre principale
+    window_x = window.winfo_x()
+    window_y = window.winfo_y()
+    window_width = window.winfo_width()
+    window_height = window.winfo_height()
+    dialog_x = window_x + (window_width - 300) // 2
+    dialog_y = window_y + (window_height - 100) // 2
+    dialog.geometry(f"+{dialog_x}+{dialog_y}")
+
+    tk.Label(dialog, text="Double three detected. Move not allowed.", font=("Arial", 12)).pack(pady=10)
+
+    # Bouton pour fermer la boîte de dialogue et continuer le jeu
+    tk.Button(dialog, text="Continue", command=dialog.destroy).pack()
+
+    dialog.transient(window)  # Rend la boîte de dialogue modale
+    dialog.grab_set()  # Empêche l'interaction avec la fenêtre principale
+    window.wait_window(dialog)  # Attend que la boîte de dialogue soit fermée
+
+
+def show_custom_winner_dialog(winner):
+    global game_over
+    game_over = True
+    dialog = tk.Toplevel(window)  # Crée une nouvelle fenêtre au-dessus de la fenêtre principale
+    dialog.title("Partie terminée")
+    dialog.geometry("300x100")  # Taille de la boîte de dialogue
+
+    # Calcul pour centrer la boîte de dialogue par rapport à la fenêtre principale
+    window_x = window.winfo_x()
+    window_y = window.winfo_y()
+    window_width = window.winfo_width()
+    window_height = window.winfo_height()
+    dialog_x = window_x + (window_width - 300) // 2
+    dialog_y = window_y + (window_height - 100) // 2
+    dialog.geometry(f"+{dialog_x}+{dialog_y}")
+
+    tk.Label(dialog, text=f"{winner.capitalize()} a gagné! \nVoulez-vous jouer une nouvelle partie?", font=("Arial", 12)).pack(pady=10)
+    tk.Button(dialog, text="Oui", command=lambda: [restart_game(), dialog.destroy()]).pack(side="left", padx=(50, 20), pady=10)
+    tk.Button(dialog, text="Non", command=window.destroy).pack(side="right", padx=(20, 50), pady=10)
+
+    dialog.transient(window)  # Indique que c'est une fenêtre secondaire
+    dialog.grab_set()  # Empêche l'interaction avec la fenêtre principale tant que la boîte de dialogue est ouverte
+    window.wait_window(dialog)  # Attend que la boîte de dialogue soit fermée
+
+
+def update_player_label():
+    player_label.config(text=f"Current Player: {current_player[0].capitalize()}")
+
+def update_capture_labels():
+    black_captures_label.config(text=f"Black captures pairs\n {captures['black'] // 2} / 5")
+    white_captures_label.config(text=f"White captures pairs\n {captures['white'] // 2} / 5")
 
 def on_resize(event):
     # Cette fonction est appelée chaque fois que la fenêtre est redimensionnée.
     print("Taille actuelle : {}x{}".format(event.width, event.height))
 
+def ai_move(board, player):
+    best_move = None
+    # TODO
+    return best_move
+
+
+
+def play(grid_x, grid_y):
+    if grid_x < 19 and grid_y < 19 and (grid_x, grid_y) not in board:
+        place_stone(canvas, board, grid_x, grid_y, current_player[0])
+        if not check_double_three(board, current_player[0], grid_x, grid_y):
+            if check_capture(canvas, board, current_player[0], grid_x, grid_y, captures):
+                update_capture_labels()
+                if captures[current_player[0]] >= 10:
+                    show_custom_winner_dialog(current_player[0])
+                    return
+            if check_win(board, current_player[0], grid_x, grid_y):
+                show_custom_winner_dialog(current_player[0])
+                return
+            current_player[0] = "white" if current_player[0] == "black" else "black"
+            update_player_label()
+        else:
+            show_double_three_dialog()
+
+def canvas_click_handler(event):
+    if game_over or current_player[0] != "black":  # Supposons que le joueur noir est humain et le blanc est l'IA:
+        return
+    margin = 20
+    cell_size = 30
+    grid_x = round((event.x - margin) / cell_size)
+    grid_y = round((event.y - margin) / cell_size)
+    play(grid_x, grid_y)
+    
+    if current_player[0] == "white":  # Tour de l'IA
+        ai_x, ai_y = ai_move(board, "white")
+        if ai_x is not None and ai_y is not None:
+            place_stone(canvas, board, ai_x, ai_y, current_player[0])
+            
 def main():
+    global window, canvas, player_label, black_captures_label, white_captures_label
     window = tk.Tk()
     window.title("Gomoku Game")
     # window.bind("<Configure>", on_resize)
     window.geometry("757x646")
-    # Empêcher le redimensionnement de la fenêtre
     window.resizable(False, False)
-    # Configurez la colonne 0 (où se trouve le canevas) pour qu'elle prenne tout l'espace supplémentaire
-    window.columnconfigure(0, weight=1)
 
-    # Configurez la colonne 1 (où se trouvent les labels) pour qu'elle ne s'étende pas inutilement, ce qui peut aider à rapprocher les labels de la grille
-    window.columnconfigure(1, weight=0)
-    canvas = tk.Canvas(window, width=800, height=800)
+    canvas = tk.Canvas(window, width=600, height=600)
     canvas.grid(row=0, column=0, rowspan=4)
-    
-    captures = {"black": 0, "white": 0}
-    black_captures_label = tk.Label(window, text="Black captures pairs: 0 / 5")
-    white_captures_label = tk.Label(window, text="White captures pairs: 0 / 5")
+    canvas.bind("<Button-1>", canvas_click_handler)
+
+    black_captures_label = tk.Label(window, text="Black captures pairs\n 0 / 5")
+    white_captures_label = tk.Label(window, text="White captures pairs\n 0 / 5")
     black_captures_label.grid(row=0, column=1, sticky="w", padx=(10,0))
     white_captures_label.grid(row=1, column=1, sticky="w")
-    
-    board = {}
-    current_player = ["black"]
-    player_label = tk.Label(window, text=f"Current Player: {current_player[0].capitalize()}", font=("Arial", 16))
-    player_label.grid(row=3, column=0, sticky="n")
+
+    player_label = tk.Label(window, text="Current Player: Black", font=("Arial", 16))
+    player_label.grid(row=4, column=0, sticky="n")
+
     draw_board(canvas)
-
-    def update_player_label():
-        player_label.config(text=f"Current Player: {current_player[0].capitalize()}")
-
-    def update_capture_labels():
-        black_captures_label.config(text=f"Black captures pairs: {captures['black'] // 2} / 5")
-        white_captures_label.config(text=f"White captures pairs: {captures['white'] // 2} / 5")
-
-    def canvas_click_handler(event):
-        margin = 20
-        cell_size = 30
-        grid_x = round((event.x - margin) / cell_size)
-        grid_y = round((event.y - margin) / cell_size)
-        
-        if (grid_x, grid_y) not in board:
-            if not check_double_three(board, current_player[0], grid_x, grid_y):
-                place_stone(canvas, board, grid_x, grid_y, current_player[0])
-                if check_capture(canvas, board, current_player[0], grid_x, grid_y, captures):
-                    update_capture_labels()
-                    if captures[current_player[0]] >= 10:
-                        show_winner(canvas, current_player[0])
-                        return
-                if check_win(board, current_player[0], grid_x, grid_y):
-                    show_winner(canvas, current_player[0])
-                    return
-                current_player[0] = "white" if current_player[0] == "black" else "black"
-                update_player_label()
-    canvas.bind("<Button-1>", canvas_click_handler)
     window.mainloop()
 
 if __name__ == "__main__":
