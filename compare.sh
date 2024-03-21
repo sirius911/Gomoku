d703135b0aeadaeb5e010ad3fee922174ac8e61b
clorin <clorin@student.42.fr>
2024-03-21 19:36:29 +0100

    refact compare

--- compare.sh
+++ compare.sh
@@ -2,13 +2,13 @@
 
 REPERTOIRE="c/"
 
-git checkout main
+git checkout $1
 
 # Lister tous les fichiers .c dans le répertoire spécifié
 # et boucler sur chaque fichier trouvé
 find $REPERTOIRE -type f -name "*.c" | while read fichier; do
-    echo "Comparaison pour $fichier entre main et threads:"
+    echo "Comparaison pour $fichier entre $1 et $2:"
     # Utiliser git diff pour comparer chaque fichier entre les branches main et threads
-    git --no-pager diff main threads -- "$fichier"
+    git --no-pager diff $1 $2 -- "$fichier"
     echo "------------------------------------------------"
 done
